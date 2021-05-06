a813526eba8e157b5195856e02e0941983c9bd9a
Mohamad Fazel <mdhesari99@gmail.com>
2021-05-07 02:57:46 +0430

    partials

---
+++ resources/views/partials/top-header.blade.php
@@ -0,0 +1,26 @@
+<div class="top-header">
+    <div class="right-header">
+        <button class="burger-btn" id="burgerBtn" onclick="openMenu()">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="logo-holder">
+            <a href="index.html">
+                <img src="assets/img/logo.png" alt="">
+            </a>
+        </div>
+        <nav class="main-menu" id="mainMenu">
+            <h2 class="d-none">منوی کاربری ارزکو</h2>
+            <ul>
+                <li><a href="filter-page.html" class="active">مقایسه قیمت</a></li>
+                <li><a href="exchanges.html">صرافی ها </a></li>
+                <li><a href="blog.html">بلاگ </a></li>
+                <li><a href="live-prices.html">قیمت لحظه ای</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="actions">
+        <a href="otp.html">
+            ثبت نام
+        </a>
+    </div>
+</div>
