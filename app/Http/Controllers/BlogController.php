477d6ce42461245bd8225777cbc73c1aa8f672b0
Mohamad Fazel <mdhesari99@gmail.com>
2021-05-07 03:39:32 +0430

    setup pagination

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,7 +10,7 @@ class BlogController extends Controller
     public function index()
     {
         return view('blog.index', [
-            'posts' => Post::published()->latest()->paginate(),
+            'posts' => Post::published()->latest()->paginate(1),
         ]);
     }
 
--- resources/views/components/blog-list.blade.php
+++ resources/views/components/blog-list.blade.php
@@ -1,5 +1,9 @@
 <div class="row blogs">
-        @foreach($posts as $post)
-            <x-blog-list-item :post="$post" :isFull="$loop->first"></x-blog-list-item>
-        @endforeach
+    @foreach($posts as $post)
+        <x-blog-list-item :post="$post" :isFull="$loop->first"></x-blog-list-item>
+    @endforeach
+
+    <div class="pagination-links my-4 mx-auto">
+        {{ $posts->links() }}
+    </div>
 </div>
