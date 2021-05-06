c002f68e9d32d901f368ea28bd06ec4774062078
Mohamad Fazel <mdhesari99@gmail.com>
2021-05-07 02:37:59 +0430

    add version param config

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    'version' => '0.10.0',
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
