899d895ba73a66a756d0b532fa7528311daa366f
Mohamad Fazel Hesari <mdhesari99@gmail.com>
2021-06-13 13:41:09 +0430

    get rid of some routes

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+// Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::prefix('blog')->name('blog.')->group(function () {
+Route::prefix('/')->name('blog.')->group(function () {
     Route::get('/', [BlogController::class, 'index'])->name('home');
     Route::get('/{post}', [BlogController::class, 'show'])->name('show');
 });
 
-Route::prefix('exchanges')->name('exchanges.')->group(function () {
-    Route::get('/exchanges', [ExchangeController::class, 'index'])->name('home');
-});
+// Route::prefix('exchanges')->name('exchanges.')->group(function () {
+//     Route::get('/exchanges', [ExchangeController::class, 'index'])->name('home');
+// });
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
