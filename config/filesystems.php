a70cb862877fbf36311c17397427674130fb95c5
Mohamad Fazel <mdhesari99@gmail.com>
2021-08-14 19:03:15 +0430

    setup backup system

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
@@ -52,6 +52,10 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'dropbox' => [
+            'driver' => 'dropbox',
+            'token'  => env('DROPBOX_TOKEN'),
+        ],
     ],
 
     /*
