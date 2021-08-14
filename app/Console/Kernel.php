9ace4106b5cdbd4c09a62b5e6841bd53754891b8
Mohamad Fazel <mdhesari99@gmail.com>
2021-08-14 19:28:10 +0430

    sitemap generator setup & command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('scrap:exchanges')->daily();
-        // $schedule->command('sitemap:generate')->daily();
+        $schedule->command('sitemap:generate')->daily();
         $schedule->command('backup:clean')->weekly()->at('01:00');
         $schedule->command('backup:run')->weekly()->at('01:30');
         $schedule->command('backup:run --only-db')->daily()->at('01:30');
