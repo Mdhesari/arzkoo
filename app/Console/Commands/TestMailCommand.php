7566947310b491fe55c15a5bc0c7f5d4b20ed938
Mohamad Hesari <mdhesari99@gmail.com>
2022-01-10 22:25:10 +0330

    test mail

---
+++ app/Console/Commands/TestMailCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class TestMailCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'arzkoo:test-mail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Mail::raw('Hello World!', function($msg) {$msg->to('mdhesari99@gmail.com')->subject('Test Email'); });
+
+        return 0;
+    }
+}
