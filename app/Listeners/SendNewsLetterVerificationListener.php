248558c12f6597849d0aec61944c3fd72fddf6cc
Mohamad Hesari <mdhesari99@gmail.com>
2022-01-10 23:56:40 +0330

    shoud queue news letter

--- app/Listeners/SendNewsLetterVerificationListener.php
+++ app/Listeners/SendNewsLetterVerificationListener.php
@@ -6,7 +6,7 @@ use App\Events\NewSubmittedNewsletterForm;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class SendNewsLetterVerificationListener
+class SendNewsLetterVerificationListener implements ShouldQueue
 {
     /**
      * Create the event listener.
