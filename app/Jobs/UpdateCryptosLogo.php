a9d423679fadb7d759c212af597d2fa21e0a6042
Mohamad Hesari <mdhesari99@gmail.com>
2022-02-24 15:46:15 +0330

    update cryptos logo not queue

--- app/Jobs/UpdateCryptosLogo.php
+++ app/Jobs/UpdateCryptosLogo.php
@@ -11,7 +11,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Crypt;
 
-class UpdateCryptosLogo implements ShouldQueue
+class UpdateCryptosLogo
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
