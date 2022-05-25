9a5c312a1a46981ed0a6198373afe77289cce756
Mohamad Hesari <mdhesari99@gmail.com>
2022-05-25 18:13:10 +0430

    comments are empty

--- resources/views/livewire/comments-list.blade.php
+++ resources/views/livewire/comments-list.blade.php
@@ -3,57 +3,63 @@
         <h3>نظرات</h3>
     </div>
     <div class="reviews">
-        @foreach($comments as $comment)
-            <livewire:comment-item :comment="$comment"/>
-        @endforeach
+        @if($comments->total() > 0)
+            @foreach($comments as $comment)
+                <livewire:comment-item :comment="$comment"/>
+            @endforeach
+        @else
+            <div class="alert alert-info">
+                فعلا نظری برای این مطلب ثبت نشده است.
+            </div>
+        @endif
 
-{{--        <div class="review response">--}}
-{{--            <div class="avatar">--}}
-{{--                <i class="fas fa-user-circle"></i>--}}
-{{--            </div>--}}
-{{--            <div class="review-body">--}}
-{{--                <div class="review-header">--}}
-{{--                    <div class="name">--}}
-{{--                            <span>--}}
-{{--                                محمد حصاری--}}
-{{--                            </span>--}}
-{{--                    </div>--}}
-{{--                    <div class="stars">--}}
-{{--                        <i class="fa fa-star star-yellow"></i>--}}
-{{--                        <i class="fa fa-star star-yellow"></i>--}}
-{{--                        <i class="fa fa-star star-yellow"></i>--}}
-{{--                        <i class="fa fa-star star-yellow"></i>--}}
-{{--                        <i class="fa fa-star"></i>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div class="review-text">--}}
-{{--                    <p>--}}
-{{--                        طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند--}}
-{{--                        که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار--}}
-{{--                        فرما نمیدهد.--}}
-{{--                    </p>--}}
-{{--                </div>--}}
-{{--                <div class="review-footer">--}}
-{{--                    <div class="time">--}}
-{{--                            <span>--}}
-{{--                                2--}}
-{{--                            </span>--}}
-{{--                        ماه پیش--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
+        {{--        <div class="review response">--}}
+        {{--            <div class="avatar">--}}
+        {{--                <i class="fas fa-user-circle"></i>--}}
+        {{--            </div>--}}
+        {{--            <div class="review-body">--}}
+        {{--                <div class="review-header">--}}
+        {{--                    <div class="name">--}}
+        {{--                            <span>--}}
+        {{--                                محمد حصاری--}}
+        {{--                            </span>--}}
+        {{--                    </div>--}}
+        {{--                    <div class="stars">--}}
+        {{--                        <i class="fa fa-star star-yellow"></i>--}}
+        {{--                        <i class="fa fa-star star-yellow"></i>--}}
+        {{--                        <i class="fa fa-star star-yellow"></i>--}}
+        {{--                        <i class="fa fa-star star-yellow"></i>--}}
+        {{--                        <i class="fa fa-star"></i>--}}
+        {{--                    </div>--}}
+        {{--                </div>--}}
+        {{--                <div class="review-text">--}}
+        {{--                    <p>--}}
+        {{--                        طراحان سایت هنگام طراحی قالب سایت معمولا با این موضوع رو برو هستند--}}
+        {{--                        که محتوای اصلی صفحات آماده نیست. در نتیجه طرح کلی دید درستی به کار--}}
+        {{--                        فرما نمیدهد.--}}
+        {{--                    </p>--}}
+        {{--                </div>--}}
+        {{--                <div class="review-footer">--}}
+        {{--                    <div class="time">--}}
+        {{--                            <span>--}}
+        {{--                                2--}}
+        {{--                            </span>--}}
+        {{--                        ماه پیش--}}
+        {{--                    </div>--}}
+        {{--                </div>--}}
+        {{--            </div>--}}
+        {{--        </div>--}}
 
-{{--        <div class="replay">--}}
-{{--            <span>پاسخ به حصاری</span>--}}
-{{--            <form action="">--}}
-{{--                <div class="your-message">--}}
-{{--                    <textarea name="" id="" cols="30" rows="5"></textarea>--}}
-{{--                </div>--}}
-{{--                <div class="send-message">--}}
-{{--                    <button class="btn btn-default-outline" type="submit"> ارسال</button>--}}
-{{--                </div>--}}
-{{--            </form>--}}
-{{--        </div>--}}
+        {{--        <div class="replay">--}}
+        {{--            <span>پاسخ به حصاری</span>--}}
+        {{--            <form action="">--}}
+        {{--                <div class="your-message">--}}
+        {{--                    <textarea name="" id="" cols="30" rows="5"></textarea>--}}
+        {{--                </div>--}}
+        {{--                <div class="send-message">--}}
+        {{--                    <button class="btn btn-default-outline" type="submit"> ارسال</button>--}}
+        {{--                </div>--}}
+        {{--            </form>--}}
+        {{--        </div>--}}
     </div>
 </div>
