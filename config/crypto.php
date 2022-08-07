31ba974d5fcdab756611c7b2f7f4be113e4f170c
Mohamad Hesari <mdhesari99@gmail.com>
2022-08-07 20:32:28 +0430

    refactor

--- config/crypto.php
+++ config/crypto.php
@@ -2,7 +2,7 @@
 
 return [
     'coinmarketcap' => [
-        'api_key' => env('COINMARKETCAP_API_KEY'),
+        'api_key'     => env('COINMARKETCAP_API_KEY'),
         'unsupported' => array_replace(
             explode(',', env('COINMARKETCAP_UNSUPPORTED')),
             ['XDCE', 'NANO', 'PAX']
