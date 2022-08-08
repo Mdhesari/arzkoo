8d037ea8da6c6a3930346219946863dd9eb8bf96
Mohamad Hesari <mdhesari99@gmail.com>
2022-08-08 19:58:23 +0430

    fix swiper

--- resources/views/components/searchly.blade.php
+++ resources/views/components/searchly.blade.php
@@ -51,27 +51,26 @@
             </div>
             <div class="swiper live-prices live-prices-slider row m-30">
                 <div class="swiper-wrapper">
-                @php $countBestEx = 0 @endphp
-                @foreach ($favCryptos as $cry)
-                    @php $best = $cry->bestBuyExchange->first() @endphp
+                    @php $countBestEx = 0 @endphp
+                    @foreach ($favCryptos as $cry)
+                        @php $best = $cry->bestBuyExchange->first() @endphp
 
-                    @if ($best && $countBestEx < 5)
-                        <div  class="swiper-slide">
-                            <div class="item">
-                                <div class="icon">
-                                    {{--                                        <i class="fab fa-btc"></i>--}}
-                                    <img src="{{ $cry->logo_full_url }}" alt="{{ $cry->name }}">
+                        @if ($best && $countBestEx < 5)
+                            <div class="swiper-slide">
+                                <div class="item">
+                                    <div class="icon">
+                                        <img src="{{ $cry->logo_full_url }}" alt="{{ $cry->name }}">
+                                    </div>
+                                    <div class="detail">
+                                        <strong>{{ $best->irr_buy_price_formatted }}</strong>
+                                        <p>بهترین قیمت {{ $cry->name }} در {{ $best->persian_title }}</p>
+                                    </div>
+                                    @php ++$countBestEx @endphp
                                 </div>
-                                <div class="detail">
-                                    <strong>{{ $best->irr_buy_price_formatted }}</strong>
-                                    <p>بهترین قیمت {{ $cry->name }} در {{ $best->persian_title }}</p>
-                                </div>
-                                @php ++$countBestEx @endphp
                             </div>
-                        </div>
-                    @endif
+                        @endif
 
-                @endforeach
+                    @endforeach
                 </div>
             </div>
         </div>
@@ -194,31 +193,27 @@
         })
 
         var swiper = new Swiper(".live-prices-slider", {
-    slidesPerView: 5,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    },
-    autoplay: {
-      delay: 5000
-    },
-    breakpoints: {
-      // when window width is <= 768px
-      992: {
-        slidesPerView: 5,
-        spaceBetweenSlides: 10
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetweenSlides: 30
-      },
-      100: {
-        slidesPerView: 1,
-        spaceBetweenSlides: 50
-      }
-      // when window width is <= 999px
-    }
-  })
+            slidesPerView: 5,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 5000
+            },
+            breakpoints: {
+                // when window width is <= 768px
+                992: {
+                    slidesPerView: 5,
+                    spaceBetweenSlides: 10
+                },
+                768: {
+                    slidesPerView: 3,
+                    spaceBetweenSlides: 30
+                },
+                100: {
+                    slidesPerView: 1,
+                    spaceBetweenSlides: 50
+                }
+                // when window width is <= 999px
+            }
+        })
     </script>
 @endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,7 @@
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
     <script src="{{ mix('js/app.js') }}" defer></script>
 
     @stack('add_scripts')
